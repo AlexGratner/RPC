e9b52e780fef67948cef10da93edc513dc62e5d4
Alexander <gratner@kth.se>
2015-12-03 11:22:44 +0100

    Removing all

--- setPath.m
+++
@@ -1,2 +0,0 @@
-javaaddpath('D:\Documents\MATLAB Files\DiDi\Includes\External\jgraphx\lib\jgraphx.jar');
-javaaddpath('D:\Documents\MATLAB Files\DiDi\Includes\External\customjgraphx\customjgraphx\dist\customjgraphx.jar');
